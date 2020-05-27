99697d7ebf43be5fdc13096cc6528c53887411d7
Daniel Matoz <fdamatoz@gmail.com>
2020-05-26 20:59:59 -0500

    Session 3

---
+++ cuda/gpumd/md/build/CMakeFiles/nvccmodule.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alfios/Documents/codes/ABPTutorial/cuda/gpumd/md/src/from_json.cpp" "/home/alfios/Documents/codes/ABPTutorial/cuda/gpumd/md/build/CMakeFiles/nvccmodule.dir/src/from_json.cpp.o"
+  "/home/alfios/Documents/codes/ABPTutorial/cuda/gpumd/md/src/pybindexport.cpp" "/home/alfios/Documents/codes/ABPTutorial/cuda/gpumd/md/build/CMakeFiles/nvccmodule.dir/src/pybindexport.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "nvccmodule_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/cuda/include"
+  "../src"
+  "../external/pybind11/include"
+  "/home/alfios/.miniconda3/envs/ABPGPU/include/python3.7m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
